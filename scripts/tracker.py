8e7361b69e72ae854c4eac0270e15ace99a57fc3
jovanzac <jovanzac@gmail.com>
2024-03-03 23:40:30 +0530

    began implementation of deepsort tracking. Created a wrapper around deepsort. Also restructured files a bit

---
+++ scripts/tracker.py
@@ -0,0 +1,67 @@
+from deep_sort.deep_sort.tracker import Tracker as DeepSortTracker
+from deep_sort.tools import generate_detections
+from deep_sort.deep_sort import nn_matching
+from deep_sort.deep_sort.detection import Detection
+import numpy as np
+
+
+class Tracker :
+    tracker = None
+    encoder = None
+    tracks = None
+    
+    def __init__(self) :
+        max_cosine_dist = 0.4
+        nn_budget = None
+        
+        encoder_model_file = "models/mars-small128.pb"
+        
+        metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_dist, nn_budget)
+        self.tracker = DeepSortTracker(metric)
+        self.encoder = generate_detections.create_box_encoder(encoder_model_file, batch_size=1)
+        
+    def update(self, frame, detections) :
+        if len(detections) == 0 :
+            self.tracker.predict()
+            self.tracker.update([])
+            self.update_tracks()
+            return
+        
+        bboxes = np.asarray([d[:-1] for d in detections])
+        bboxes[:, 2:] = bboxes[:, 2:] - bboxes[:, 0:2]
+        print(f"bboxes: {bboxes}   len(bboxes): {bboxes}")
+        scores = [d[-1] for d in detections]
+        
+        features = self.encoder(frame, bboxes)
+        
+        dets = []
+        for bbox_id, bbox in enumerate(bboxes) :
+            dets.append(Detection(bbox, scores[bbox_id], features[bbox_id]))
+            
+        self.tracker.predict()
+        self.tracker.update(dets)
+        self.update_tracks()
+        
+    
+    def update_tracks(self) :
+        tracks = []
+        for track in self.tracker.tracks :
+            if not track.is_confirmed() or track.time_since_update > 1 :
+                continue
+            bbox = track.to_tlbr()
+            
+            id = track.track_id
+            
+            tracks.append(Track(id, bbox))
+            
+        self.tracks = tracks
+        
+    
+class Track :
+    track_id = None
+    bbox = None
+    
+    
+    def __init__(self, t_id, bbox) :
+        self.track_id = t_id
+        self.bbox = bbox
---
+++ scripts/yolo_detection.py
@@ -0,0 +1,124 @@
+import cv2
+from ultralytics import YOLO
+
+
+class Detection :
+    MODEL = "yolov8l.pt"
+    VIDEO_FILE = "vid_files/vehicle-counting1.mp4"
+
+    def __init__(self) :
+        print("Loading model...")
+        self.model = YOLO(self.MODEL)
+        print("Done Loading! Now Fusing Model...")
+        self.model.fuse()
+        print("Done with the fuse!")
+        
+        self.object_classes = self.model.model.names
+    
+
+    def resize_n_write_video(self, out_vid, in_vid=VIDEO_FILE) :
+        """Can be used to resize the frames of a specified video and
+        write the new frames out to a new location.
+
+        Args:
+            out_vid (str): filename for the new video file. Must have the extension .mp4
+            in_vid (str, optional): Defaults to VIDEO_FILE. Specified the video file to resize
+        """
+        output = cv2.VideoWriter(out_vid, cv2.VideoWriter_fourcc(*'mp4v'), 25, (1459, 821))
+        cap = cv2.VideoCapture(in_vid)
+        while cap.isOpened() :
+            ret, frame = cap.read()
+            if ret == False :
+                print("Exiting video player")
+                break
+            
+            # Resize the frame
+            print(type(frame.shape[0]))
+            print(f"frame width: {frame.shape[0]}   height: {frame.shape[1]}")
+            resized_frame = cv2.resize(frame, (0,0), fx=0.38, fy=0.38)
+            print(f"new frame width: {resized_frame.shape[0]}   height: {resized_frame.shape[1]}")
+            
+            # writing the new frame in output 
+            output.write(resized_frame)
+            cv2.imshow("Frame", resized_frame)
+            if cv2.waitKey(25) & 0xFF==ord("q") :
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+    def display_vid_n_predict(self, invid=VIDEO_FILE, speed_engine=False) :
+        """Simply plays a video file
+
+        Args:
+            invid (str, optional): Video file to play. Defaults to VIDEO_FILE.
+        """
+        cap = cv2.VideoCapture(invid)
+        while cap.isOpened() :
+            ret, frame = cap.read()
+            if ret == False :
+                print("Exiting video player")
+                break
+            
+            if speed_engine :
+                # Use yolov8 to perform vehicle detection
+                detections = self.object_detection_on_vid(frame)
+                # Annotate and draw boxes around vehicles in the frame
+                frame = self.annotate_vehicles(frame, detections.boxes)
+                
+            
+            cv2.imshow("Frame", frame)
+            if cv2.waitKey(25) & 0xFF==ord("q") :
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+        
+        
+    def object_detection_on_vid(self, frame) :
+            detections = self.model(frame)[0]
+            return detections
+        
+    
+    def bounding_box_color(self, label) :
+        if label == 0 : # Person
+            color = (255, 85, 45) # purple
+        elif label == 1 : # Bicycle
+            color = (51, 34, 164) # forrest green
+        elif label == 2 : # Car
+            color = (175, 222, 82) # pink
+        elif label == 3 :  # Motobike
+            color = (255, 0, 204) # teal
+        elif label == 5 :  # Bus
+            color = (255, 0, 149) # darker teal
+        elif label == 7 : # Truck
+            color = (71, 246, 227) # gold
+        else:
+            color = (255, 0, 0)
+        return color
+    
+    
+    def annotate_vehicles(self, frame, boxes) :
+        # Initializing the font configurations
+        FONT = cv2.FONT_HERSHEY_COMPLEX_SMALL
+        FONTSCALE = 1
+        THICKNESS = 2
+        
+        # xy locations and classes of all vehicles
+        xyxy = boxes.xyxy.tolist()
+        classes = boxes.cls.tolist()
+        for vehicle, vehicle_class in zip(xyxy, classes) :
+            vehicle = [int(i) for i in vehicle]
+            # color correspondign to the vehicle
+            color = self.bounding_box_color(vehicle_class)
+            frame = cv2.rectangle(frame, vehicle[:2], vehicle[2:], color, 2)
+            frame = cv2.putText(frame, f'{self.object_classes[vehicle_class]}', (vehicle[0], vehicle[1]-5), FONT,  
+                   FONTSCALE, color, THICKNESS, cv2.LINE_AA)
+        
+        return frame
+
+
+if __name__ == "__main__" :
+    system = Detection()
+    system.display_vid_n_predict(speed_engine=True)
