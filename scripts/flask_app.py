c1d815493ffac007eac07b1b033fec03dad884c4
jovanzac <jovanzac@gmail.com>
2024-06-24 11:19:38 +0530

    changed template folder

--- scripts/flask_app.py
+++ scripts/flask_app.py
@@ -6,7 +6,7 @@ from scripts import db_manager, ai_cam
 # app = Flask(__name__)
 app = Flask(__name__, static_url_path='',
                   static_folder='dist',
-                  template_folder='dist')
+                  template_folder='templates')
 CORS(app)
 
 
